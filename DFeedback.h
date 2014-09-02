ca8f59e5630cd67893943e19fc048beec47f1c53
Stephan Michels <stephan.michels@gmail.com>
2014-09-02 20:56:25 +0200

    Try to create a framework.

---
+++ DFeedback.h
@@ -0,0 +1,13 @@
+//
+//  DFeedback.h
+//  DFeedback
+//
+//  Created by Stephan Michels on 02.09.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#import <DFeedback/DFApplication.h>
+#import <DFeedback/DFCrashReportWindowController.h>
+#import <DFeedback/DFFeedbackWindowController.h>
